c46c9e5a83d8858f7617217b0f72eb5b50d72e8a
soumame <46675982+soumame@users.noreply.github.com>
2024-05-29 22:43:15 +0800

    chore: Update package.json and index.ts - remove nodemon to use tsx

--- index.ts
+++ index.ts
@@ -1,11 +1,27 @@
+//entry point of the application
+
 import express from "express";
 require("dotenv").config(); // Load .env file
 const app = express();
 const port = process.env.PORT;
+
+//frontend(Browser with Vite)
 app.get("/", (req, res) => {
   console.log("new request!");
   res.send("<h1>Hello</h1>");
 });
+
+//Minecraft / Minecraft Edu Websocket join code
+app.get("/:code", (req, res) => {
+  const code = req.params.code;
+});
+
+//backend(Server)
+app.get("/api", (req, res) => {
+  res.send({ message: "Hello from the backend!" });
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
