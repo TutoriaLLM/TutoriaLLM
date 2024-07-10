8388b8c50e0ee64ce5e9baa373e7b969faaf7916
soumame <sou.tokumaru@gmail.com>
2024-07-10 15:02:42 +0800

    💄 Adjust padding and placeholder for dialogue component

--- src/client/components/BlocklyEditor/dialogue/index.tsx
+++ src/client/components/BlocklyEditor/dialogue/index.tsx
@@ -5,8 +5,10 @@ import type { Dialogue } from "../../../../type";
 import { currentSessionState } from "../../../state";
 import TextBubble from "./parts/textbubble";
 import TutorialPicker from "./tutorialPicker";
+import { useTranslation } from "react-i18next";
 
 export default function DialogueView() {
+	const { t } = useTranslation();
 	const [sessionState, setSessionState] = useAtom(currentSessionState);
 	const [message, setMessage] = useState("");
 	const messagesEndRef = useRef<HTMLDivElement>(null);
@@ -86,10 +88,10 @@ export default function DialogueView() {
 							<span className="bg-gray-200 rounded-full p-2">
 								<Bot />
 							</span>
-							<p className="text-xs">AI</p>
+							<p className="text-xs">{t("textbubble.ai")}</p>
 						</div>
 						<div className="rounded-2xl rounded-bl-none bg-gray-300 text-gray-800 p-3 shadow max-w-xs">
-							<p className="prose">Replying...</p>
+							<p className="prose">{t("textbubble.replying")}</p>
 						</div>
 					</div>
 				)}
@@ -97,23 +99,17 @@ export default function DialogueView() {
 			</div>
 			<div className="w-full p-2">
 				<div className="flex items-center bg-white shadow gap-2 p-2 rounded-2xl">
-					<button
-						type="button"
-						className="p-2 bg-gray-200 text-gray-800 rounded-2xl hover:bg-gray-300 flex"
-					>
-						<PlusCircle />
-					</button>
 					<form className="flex w-full gap-2" onSubmit={sendMessage}>
 						<input
 							type="text"
-							placeholder="Ask anything..."
-							className="flex-1 px-4 py-2 border rounded-2xl outline-none focus:ring-2 focus:ring-blue-500"
+							placeholder={t("textbubble.ask")}
+							className="flex-1 p-3 border rounded-2xl outline-none focus:ring-2 focus:ring-blue-500"
 							value={message}
 							onChange={(e) => setMessage(e.target.value)}
 						/>
 						<button
 							type="submit" // buttonのtypeを'submit'に変更
-							className={`p-2 text-white rounded-2xl flex ${
+							className={`p-3 text-white rounded-2xl flex ${
 								message === ""
 									? "bg-gray-300 transition"
 									: "bg-sky-600 hover:bg-sky-700 transition"
