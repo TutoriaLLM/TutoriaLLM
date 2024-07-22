bf4129ce8dda904972539541084c02d7381b4124
soumame <sou.tokumaru@gmail.com>
2024-07-22 16:05:09 +0800

    🎨 Fixed port for Redis

--- src/server/db/session.ts
+++ src/server/db/session.ts
@@ -14,7 +14,7 @@ export const sessionDB = redis.createClient({
 	password: process.env.REDIS_PASSWORD,
 	socket: {
 		host: process.env.REDIS_HOST,
-		port: process.env.REDIS_PORT as unknown as number,
+		port: 6379,
 	},
 });
 await sessionDB.connect();
