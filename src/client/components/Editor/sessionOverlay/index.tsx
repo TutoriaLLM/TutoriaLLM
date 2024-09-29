892b601591522891186483361278b07b9786b6d3
soumame <sou.tokumaru@gmail.com>
2024-09-29 10:18:59 +0800

    🐛 Update language setting

--- src/client/components/Editor/sessionOverlay/index.tsx
+++ src/client/components/Editor/sessionOverlay/index.tsx
@@ -26,7 +26,15 @@ export default function SessionPopup(props: {
 
 	useEffect(() => {
 		if (languageToStart === "") {
-			setLanguageToStart(navigator.language ?? i18n.language);
+			const detectedLanguage = navigator.language || i18n.language;
+			const baseLanguage = detectedLanguage.split("-")[0]; // Get the base language code
+			const supportedLanguages = i18n.languages;
+			console.log("Detected language: ", baseLanguage);
+			console.log("Supported languages: ", supportedLanguages);
+			const language = supportedLanguages.includes(baseLanguage)
+				? baseLanguage
+				: "en"; // Default to English if the detected language is not supported
+			setLanguageToStart(language);
 		}
 	}, [languageToStart, setLanguageToStart]);
 
