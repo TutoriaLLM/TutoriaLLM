46c0324b26c5e8572cd029cad75d2640e5e90c25
soumame <sou.tokumaru@gmail.com>
2024-09-29 10:19:35 +0800

    📝 Add "about this app" section on starting screen

--- src/client/components/Editor/sessionOverlay/index.tsx
+++ src/client/components/Editor/sessionOverlay/index.tsx
@@ -56,13 +56,6 @@ export default function SessionPopup(props: {
 								<Dialog.Description className="text-md font-medium text-gray-600">
 									{t("session.welcome")}
 								</Dialog.Description>
-								{/* <a
-              href="about"
-              className=" p-2 rounded-xl bg-gray-200 hover:bg-gray-300 flex gap-1.5 justify-center items-center"
-            >
-              <HelpCircle className="w-6 h-6" />
-              {t("session.about")}
-            </a> */}
 							</div>
 
 							<div className="bg-gray-50 rounded-3xl shadow p-3 w-full">
