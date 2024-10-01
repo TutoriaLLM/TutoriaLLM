b3e8aa36602586dc0c99ae39e78505beacf4c9a5
soumame <sou.tokumaru@gmail.com>
2024-10-01 18:44:46 +0800

    ⚡️ 📈 Sentry Support

--- src/server/telemetry.ts
+++ src/server/telemetry.ts
@@ -1,3 +1,8 @@
+//sentry support with opentelemetry
+
+import Sentry from "@sentry/node";
+import { GenericPoolInstrumentation } from "@opentelemetry/instrumentation-generic-pool";
+
 import * as opentelemetry from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
@@ -5,19 +10,30 @@ import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
 import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { Resource } from "@opentelemetry/resources";
 
-const sdk = new opentelemetry.NodeSDK({
-	resource: new Resource({
-		"service.name": "tutoriallm",
-	}),
-	traceExporter: new OTLPTraceExporter({
-		url: process.env.TELEMETRY_EXPORT_URL || undefined,
-	}),
-	metricReader: new PeriodicExportingMetricReader({
-		exporter: new OTLPMetricExporter({
-			url: process.env.TELEMETRY_EXPORT_URL || undefined,
-		}),
-	}),
-	instrumentations: [getNodeAutoInstrumentations()],
+// Make sure to call "Sentry.init" BEFORE initializing the OpenTelemetry SDK
+Sentry.init({
+	dsn: process.env.SENTRY_DSN,
+	tracesSampleRate: 1.0,
 });
 
-sdk.start();
+Sentry.addOpenTelemetryInstrumentation(new GenericPoolInstrumentation());
+
+// Sentryが有効でない場合は、ローカルでのテレメトリを利用するため、OpenTelemetry SDKを初期化する
+if (!process.env.SENTRY_DSN) {
+	const sdk = new opentelemetry.NodeSDK({
+		resource: new Resource({
+			"service.name": "tutoriallm",
+		}),
+		traceExporter: new OTLPTraceExporter({
+			url: process.env.TELEMETRY_EXPORT_URL || undefined,
+		}),
+		metricReader: new PeriodicExportingMetricReader({
+			exporter: new OTLPMetricExporter({
+				url: process.env.TELEMETRY_EXPORT_URL || undefined,
+			}),
+		}),
+		instrumentations: [getNodeAutoInstrumentations()],
+	});
+
+	sdk.start();
+}
