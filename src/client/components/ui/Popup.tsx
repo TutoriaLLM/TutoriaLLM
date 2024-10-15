267581706ec377c510074519785aeed27ac1149d
soumame <sou.tokumaru@gmail.com>
2024-10-15 18:49:11 +0800

    💄 Animated popup

--- src/client/components/ui/Popup.tsx
+++ src/client/components/ui/Popup.tsx
@@ -14,14 +14,14 @@ export default function Popup(props: {
 	return (
 		showPopup && (
 			<Dialog.Root open={showPopup} onOpenChange={props.onClose}>
-				<Dialog.Overlay className="fixed w-full h-full inset-0 z-[997] bg-gray-200/10 backdrop-blur-md p-2">
+				<Dialog.Overlay className="fixed w-full h-full inset-0 z-[997] bg-gray-200/10 backdrop-blur-md p-2 data-[state=open]:animate-fade-in data-[state=close]:animate-fade-out">
 					<VisuallyHidden.Root>
 						<Dialog.Title>Popup</Dialog.Title>
 						<Dialog.Description>Popup contents</Dialog.Description>
 					</VisuallyHidden.Root>
 
 					<Dialog.Content asChild>
-						<div className="fixed flex flex-col max-w-6xl w-full max-h-[80vh] gap-3 bg-white rounded-2xl p-5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[998] font-semibold overflow-y-auto">
+						<div className="fixed flex flex-col max-w-6xl w-full max-h-[80vh] gap-3 bg-white rounded-2xl p-5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[998] font-semibold overflow-y-auto data-[state=open]:animate-fade-in data-[state=close]:animate-fade-out">
 							{props.Content}
 						</div>
 					</Dialog.Content>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,31 @@ export default {
 					"0%,70%,100%": { opacity: "1" },
 					"20%,50%": { opacity: "0" },
 				},
+				"fade-in": {
+					from: {
+						opacity: "0",
+						filter: "blur(10px)",
+					},
+					to: {
+						opacity: "1",
+						filter: "blur(0)",
+					},
+				},
+				"fade-out": {
+					from: {
+						opacity: "1",
+						filter: "blur(0)",
+					},
+					to: {
+						opacity: "0",
+						filter: "blur(10px)",
+					},
+				},
 			},
 			animation: {
 				"caret-blink": "caret-blink 1.2s ease-out infinite",
+				"fade-in": "fade-in 0.15s ease-in forwards",
+				"fade-out": "fade-out 0.15s ease-out forwards",
 			},
 		},
 	},
