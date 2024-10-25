6cb557fbf7d078b2a2eb0001e3399b7c3adcb73e
soumame <sou.tokumaru@gmail.com>
2024-10-25 17:47:04 +0800

    ✨ Relative position block for MInecraft Extension

---
+++ src/extensions/Minecraft-Core/blocks/position/relativePos.ts
@@ -0,0 +1,55 @@
+import { inputs } from "blockly";
+import { Order, javascriptGenerator } from "blockly/javascript";
+
+export const block = {
+	type: "ext_minecraft_var_xyzRelativePosition",
+	message0: "%{BKY_MINECRAFT_VAR_XYZRELATIVEPOSITION}",
+	colour: "#a855f7",
+	args0: [
+		{
+			type: "input_value",
+			name: "PosX",
+			check: "Number",
+		},
+		{
+			type: "input_value",
+			name: "PosY",
+			check: "Number",
+		},
+		{
+			type: "input_value",
+			name: "PosZ",
+			check: "Number",
+		},
+	],
+	inputsInline: true, // インライン入力を有効にする
+	output: "Position",
+};
+
+export function code() {
+	javascriptGenerator.forBlock.ext_minecraft_var_xyzRelativePosition = (
+		block,
+		generator,
+	) => {
+		const PositionX = generator.valueToCode(block, "PosX", Order.ATOMIC);
+		const PositionY = generator.valueToCode(block, "PosY", Order.ATOMIC);
+		const PositionZ = generator.valueToCode(block, "PosZ", Order.ATOMIC);
+		const code = /* javascript */ `
+		{
+			x: ${PositionX} + minecraftWorldState.player.position.x,
+			y: ${PositionY} + minecraftWorldState.player.position.y,
+			z: ${PositionZ} + minecraftWorldState.player.position.z,
+		}
+		`;
+		return [code, Order.ATOMIC];
+	};
+}
+
+export const locale = {
+	en: {
+		MINECRAFT_VAR_XYZRELATIVEPOSITION: "Relative Position ~%1 ~%2 ~%3",
+	},
+	ja: {
+		MINECRAFT_VAR_XYZRELATIVEPOSITION: "相対座標 ~%1 ~%2 ~%3",
+	},
+};
