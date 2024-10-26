405880e0a6131825ae8caffd28a9582d7fc0e10f
soumame <sou.tokumaru@gmail.com>
2024-10-26 19:02:12 +0800

    🔥 delete unused comment

--- src/server/tutorials/tags.ts
+++ src/server/tutorials/tags.ts
@@ -20,48 +20,6 @@ tagsAPI.get("/", async (req, res) => {
 	}
 });
 
-// 複数タグの絞り込み機能でチュートリアルを取得
-// tagsAPI.get("/filter", async (req, res) => {
-// 	const tagNames = req.query.tags as string[];
-// 	if (!tagNames || !Array.isArray(tagNames) || tagNames.length === 0) {
-// 		return res
-// 			.status(400)
-// 			.json({ error: "Tags parameter is required and should be an array" });
-// 	}
-
-// 	try {
-// 		const tagRecords = await db
-// 			.select()
-// 			.from(tags)
-// 			.where(inArray(tags.name, tagNames));
-// 		if (tagRecords.length !== tagNames.length) {
-// 			return res.status(404).json({ error: "One or more tags not found" });
-// 		}
-
-// 		const tagIds = tagRecords.map((tag) => tag.id);
-
-// 		const tutorialIds = await db
-// 			.select({ tutorialId: tutorialsTags.tutorialId })
-// 			.from(tutorialsTags)
-// 			.where(inArray(tutorialsTags.tagId, tagIds));
-
-// 		const tutorialList = await db
-// 			.select()
-// 			.from(tutorials)
-// 			.where(
-// 				inArray(
-// 					tutorials.id,
-// 					tutorialIds.map((t) => t.tutorialId),
-// 				),
-// 			);
-
-// 		res.json(tutorialList);
-// 	} catch (error) {
-// 		console.error("Error fetching tutorials by tags:", error);
-// 		res.status(500).json({ error: "Failed to fetch tutorials by tags" });
-// 	}
-// });
-
 // 404 handler
 tagsAPI.all("*", (req, res) => {
 	res.status(404).send("Not Found");
