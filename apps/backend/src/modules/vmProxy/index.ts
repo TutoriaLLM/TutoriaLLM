797b9ade0d41164f11a1547dd2317b0889c87dd1
soumame <sou.tokumaru@gmail.com>
2024-12-06 17:55:53 +0800

    🐛 Update proxy path

--- apps/backend/src/modules/vmProxy/index.ts
+++ apps/backend/src/modules/vmProxy/index.ts
@@ -1,6 +1,6 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { Hono } from "hono";
-let vmPort = 3001;
+let vmPort = 3002;
 if (process.env.VM_PORT) {
 	const basePort = Number.parseInt(process.env.VM_PORT, 10); // 10進数として解釈
 	if (!Number.isNaN(basePort)) {
@@ -9,7 +9,11 @@ if (process.env.VM_PORT) {
 	}
 }
 const app = new Hono().get("*", (c) => {
-	return fetch(`http://localhost:${vmPort}/`, c.req);
+	console.log("Proxying to VM", vmPort);
+	// https:.../vm/を正規表現で削除してパスを作成
+	const newPath = c.req.path.replace(/^\/vm\//, "/");
+	console.log("path", newPath);
+	return fetch(`http://localhost:${vmPort}${newPath}`, c.req);
 });
 
 export default app;
