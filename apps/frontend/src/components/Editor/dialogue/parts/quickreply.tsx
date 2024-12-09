5ec60f6d3fdfde9a208c989bb23054bdbda87526
soumame <sou.tokumaru@gmail.com>
2024-12-09 14:19:39 +0800

    🚧 Refactor QuickReply component to use translation function for replies

--- apps/frontend/src/components/Editor/dialogue/parts/quickreply.tsx
+++ apps/frontend/src/components/Editor/dialogue/parts/quickreply.tsx
@@ -1,4 +1,3 @@
-import { i18nParser } from "@/utils/i18nParser";
 import type React from "react";
 import { useTranslation } from "react-i18next";
 
@@ -8,6 +7,13 @@ interface QuickReplyProps {
 }
 
 const QuickReply: React.FC<QuickReplyProps> = ({ onReply, quickReplies }) => {
+	const { t } = useTranslation();
+
+	const handleReply = (reply: string) => {
+		const value = t(reply);
+		onReply(value);
+	};
+
 	return (
 		<div className="flex w-full gap-2 mx-4">
 			{quickReplies?.map((reply) => (
@@ -15,9 +21,9 @@ const QuickReply: React.FC<QuickReplyProps> = ({ onReply, quickReplies }) => {
 					type="button"
 					key={reply}
 					className="bg-gray-200 text-nowrap text-sm md:text-md text-gray-700 hover:bg-sky-200 transition-all p-1.5 md:p-2 font-medium rounded-full"
-					onClick={() => onReply(reply)}
+					onClick={() => handleReply(reply)}
 				>
-					{i18nParser(reply)}
+					{t(reply)}
 				</button>
 			))}
 		</div>
