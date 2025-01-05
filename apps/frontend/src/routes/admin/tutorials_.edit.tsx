b75785fd354167749a16f5d01cab9755fa3f2172
soumame <sou.tokumaru@gmail.com>
2025-01-05 15:36:23 +0800

    🚧 🎨 💄 Building new design system with tailwind/shadcn

--- apps/frontend/src/routes/admin/tutorials_.edit.tsx
+++ apps/frontend/src/routes/admin/tutorials_.edit.tsx
@@ -1,46 +1,46 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { z } from 'zod'
-import TutorialEditor from '@/components/features/admin/TutorialEditor'
-import { queryOptions, useSuspenseQuery } from '@tanstack/react-query'
-import { queryClient } from '@/main'
-import { getSpecificTutorial } from '@/api/admin/tutorials'
+import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
+import TutorialEditor from "@/components/features/admin/TutorialEditor";
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
+import { queryClient } from "@/main";
+import { getSpecificTutorial } from "@/api/admin/tutorials";
 
 const editTargetSchema = z.object({
-  id: z.number(),
-})
+	id: z.number(),
+});
 
 const tutorialQueryOptions = (id: number) =>
-  queryOptions({
-    queryKey: ['tutorials', id],
-    queryFn: () => getSpecificTutorial({ id }),
-  })
-export const Route = createFileRoute('/admin/tutorials_/edit')({
-  component: RouteComponent,
-  validateSearch: editTargetSchema,
-  beforeLoad: ({ search }) => ({
-    search: search,
-  }),
-  loader: async ({ context: { search } }) => {
-    async function getTutorialData() {
-      if (!search.id) {
-        return null
-      }
-      const tutorial = await queryClient.ensureQueryData(
-        tutorialQueryOptions(search.id),
-      )
-      return tutorial
-    }
-    return await getTutorialData()
-  },
-  shouldReload: true,
-})
+	queryOptions({
+		queryKey: ["tutorials", id],
+		queryFn: () => getSpecificTutorial({ id }),
+	});
+export const Route = createFileRoute("/admin/tutorials_/edit")({
+	component: RouteComponent,
+	validateSearch: editTargetSchema,
+	beforeLoad: ({ search }) => ({
+		search: search,
+	}),
+	loader: async ({ context: { search } }) => {
+		async function getTutorialData() {
+			if (!search.id) {
+				return null;
+			}
+			const tutorial = await queryClient.ensureQueryData(
+				tutorialQueryOptions(search.id),
+			);
+			return tutorial;
+		}
+		return await getTutorialData();
+	},
+	shouldReload: true,
+});
 
 function RouteComponent() {
-  const search = Route.useSearch()
-  const { data: tutorial } = useSuspenseQuery(tutorialQueryOptions(search.id))
-  return (
-    <div className="">
-      <TutorialEditor tutorial={tutorial || null} />
-    </div>
-  )
+	const search = Route.useSearch();
+	const { data: tutorial } = useSuspenseQuery(tutorialQueryOptions(search.id));
+	return (
+		<div className="">
+			<TutorialEditor tutorial={tutorial || null} />
+		</div>
+	);
 }
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 // import { visualizer } from "rollup-plugin-visualizer";
-
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
@@ -21,6 +22,11 @@ export default defineConfig(() => {
 	return {
 		optimizeDeps: {
 			include: ["blockly"],
+		},
+		css: {
+			postcss: {
+				plugins: [tailwindcss, autoprefixer],
+			},
 		},
 		plugins: [
 			react(),
