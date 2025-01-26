3f9d2ca05904dbe91856090e6e3157de5c46b163
soumame <sou.tokumaru@gmail.com>
2025-01-26 11:01:04 +0900

    🚧 ✨ Conditionally render workspace content based on connection status #100

--- apps/frontend/src/components/features/editor/index.tsx
+++ apps/frontend/src/components/features/editor/index.tsx
@@ -108,93 +108,95 @@ export function CodeEditor({
 					isTutorial={currentSession?.tutorial?.isTutorial ?? false}
 					tutorialProgress={currentSession?.tutorial?.progress ?? 0}
 				/>
-				<div className="flex-1 overflow-hidden relative">
-					<Button
-						type="button"
-						variant="secondary"
-						size="icon"
-						onClick={handleToggle}
-						className={cn(
-							"rounded-full absolute sm:hidden w-8 h-8 top-2 left-4 flex items-center justify-center transition-transform",
-							{ "rotate-180": isMenuOpen },
-						)}
-					>
-						<PanelRightClose />
-					</Button>
-					{isMobile ? (
-						<Tabs
-							defaultValue="workspaceTab"
-							value={activeTab}
-							onValueChange={(value) => setActiveTab(value as Tab)}
-							className="w-full h-full flex flex-col bg-accent"
+				{isWorkspaceConnected && (
+					<div className="flex-1 overflow-hidden relative">
+						<Button
+							type="button"
+							variant="secondary"
+							size="icon"
+							onClick={handleToggle}
+							className={cn(
+								"rounded-full absolute sm:hidden w-8 h-8 top-2 left-4 flex items-center justify-center transition-transform",
+								{ "rotate-180": isMenuOpen },
+							)}
 						>
-							<TabsList>
-								<TabsTrigger value="workspaceTab">
-									<Puzzle className="w-4 h-4" />
-									{t("editor.workspaceTab")}
-								</TabsTrigger>
-								<TabsTrigger value="dialogueTab">
-									<MessageCircleMore className="w-4 h-4" />
-									{t("editor.dialogueTab")}
-								</TabsTrigger>
-							</TabsList>
-							<TabsContent value="workspaceTab">
-								<BlocklyEditor
-									isMenuOpen={isMenuOpen}
-									blockNameToHighlight={blockNameToHighlight}
-									blockIdToHighlight={highlightedBlock}
-									language={currentSession?.language ?? "en"}
-									workspaceJson={workspaceJson ?? undefined}
-									setWorkspaceJson={setCurrentSessionWorkspace}
-								/>
-							</TabsContent>
-							<TabsContent value="dialogueTab">
-								<DialogueView
-									session={currentSession}
-									setSession={setCurrentSession}
-								/>
-							</TabsContent>
-						</Tabs>
-					) : (
-						<PanelGroup autoSaveId="workspace" direction="horizontal">
-							<Panel
-								id="workspaceArea"
-								defaultSize={75}
-								order={1}
-								maxSize={80}
-								minSize={20}
+							<PanelRightClose />
+						</Button>
+						{isMobile ? (
+							<Tabs
+								defaultValue="workspaceTab"
+								value={activeTab}
+								onValueChange={(value) => setActiveTab(value as Tab)}
+								className="w-full h-full flex flex-col bg-accent"
 							>
-								<BlocklyEditor
-									isMenuOpen={isMenuOpen}
-									blockNameToHighlight={blockNameToHighlight}
-									blockIdToHighlight={highlightedBlock}
-									language={currentSession?.language ?? "en"}
-									workspaceJson={workspaceJson ?? undefined}
-									setWorkspaceJson={setCurrentSessionWorkspace}
-								/>
-							</Panel>
-							<PanelResizeHandle className="h-full group w-3 transition bg-accent hover:bg-accent/80 active:bg-primary-foreground flex flex-col justify-center items-center gap-1">
-								<div className="py-2 px-1 z-50 flex flex-col gap-1">
-									<span className="rounded-full p-1  bg-accent-foreground group-hover:bg-accent-foreground/80 group-active:bg-primary" />
-									<span className="rounded-full p-1  bg-accent-foreground group-hover:bg-accent-foreground/80 group-active:bg-primary" />
-									<span className="rounded-full p-1  bg-accent-foreground group-hover:bg-accent-foreground/80 group-active:bg-primary" />
-								</div>
-							</PanelResizeHandle>
-							<Panel
-								id="dialogueArea"
-								defaultSize={25}
-								order={2}
-								maxSize={80}
-								minSize={20}
-							>
-								<DialogueView
-									session={currentSession}
-									setSession={setCurrentSession}
-								/>
-							</Panel>
-						</PanelGroup>
-					)}
-				</div>
+								<TabsList>
+									<TabsTrigger value="workspaceTab">
+										<Puzzle className="w-4 h-4" />
+										{t("editor.workspaceTab")}
+									</TabsTrigger>
+									<TabsTrigger value="dialogueTab">
+										<MessageCircleMore className="w-4 h-4" />
+										{t("editor.dialogueTab")}
+									</TabsTrigger>
+								</TabsList>
+								<TabsContent value="workspaceTab">
+									<BlocklyEditor
+										isMenuOpen={isMenuOpen}
+										blockNameToHighlight={blockNameToHighlight}
+										blockIdToHighlight={highlightedBlock}
+										language={currentSession?.language ?? "en"}
+										workspaceJson={workspaceJson ?? undefined}
+										setWorkspaceJson={setCurrentSessionWorkspace}
+									/>
+								</TabsContent>
+								<TabsContent value="dialogueTab">
+									<DialogueView
+										session={currentSession}
+										setSession={setCurrentSession}
+									/>
+								</TabsContent>
+							</Tabs>
+						) : (
+							<PanelGroup autoSaveId="workspace" direction="horizontal">
+								<Panel
+									id="workspaceArea"
+									defaultSize={75}
+									order={1}
+									maxSize={80}
+									minSize={20}
+								>
+									<BlocklyEditor
+										isMenuOpen={isMenuOpen}
+										blockNameToHighlight={blockNameToHighlight}
+										blockIdToHighlight={highlightedBlock}
+										language={currentSession?.language ?? "en"}
+										workspaceJson={workspaceJson ?? undefined}
+										setWorkspaceJson={setCurrentSessionWorkspace}
+									/>
+								</Panel>
+								<PanelResizeHandle className="h-full group w-3 transition bg-accent hover:bg-accent/80 active:bg-primary-foreground flex flex-col justify-center items-center gap-1">
+									<div className="py-2 px-1 z-50 flex flex-col gap-1">
+										<span className="rounded-full p-1  bg-accent-foreground group-hover:bg-accent-foreground/80 group-active:bg-primary" />
+										<span className="rounded-full p-1  bg-accent-foreground group-hover:bg-accent-foreground/80 group-active:bg-primary" />
+										<span className="rounded-full p-1  bg-accent-foreground group-hover:bg-accent-foreground/80 group-active:bg-primary" />
+									</div>
+								</PanelResizeHandle>
+								<Panel
+									id="dialogueArea"
+									defaultSize={25}
+									order={2}
+									maxSize={80}
+									minSize={20}
+								>
+									<DialogueView
+										session={currentSession}
+										setSession={setCurrentSession}
+									/>
+								</Panel>
+							</PanelGroup>
+						)}
+					</div>
+				)}
 			</div>
 		</TourProvider>
 	);
